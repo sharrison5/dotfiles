31e7ff7cd964c91ad352ad1286d6281a539d525a
Sam Harrison <28861239+sharrison5@users.noreply.github.com>
2018-07-03 10:30:19 +0200

    Update MacPorts

--- macports/.macports/save_requested.sh
+++ macports/.macports/save_requested.sh
@@ -5,4 +5,4 @@
 # MIT License: a full version of the license is included in the LICENSE file.
 
 date > "${HOME}/.macports/requested.txt"
-port echo requested | cut -d ' ' -f 1 >> "${HOME}/.macports/requested.txt"
+port echo requested | cut -d ' ' -f 1 | uniq >> "${HOME}/.macports/requested.txt"
