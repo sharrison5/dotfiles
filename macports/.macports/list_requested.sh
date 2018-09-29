592f729917f9649451f23900b06137b245f5a2b1
Sam Harrison <28861239+sharrison5@users.noreply.github.com>
2018-09-29 18:50:08 +0200

    Update and simplify MacPorts management
     + Move to a user maintained rather than auto-generated list of
       requested ports. This allows better organisation, recording of
       variants, and different machines to install different subsets.
     + list_requested.sh is now basically alias for `port echo` as we no
       longer automatically save to file.

---
+++ macports/.macports/list_requested.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# .macports/list_requested.sh
+# Sam Harrison 2018
+# MIT License: a full version of the license is included in the LICENSE file.
+
+port echo requested and active
--- macports/.macports/save_requested.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# .macports/save_requested.sh
-# Sam Harrison 2018
-# MIT License: a full version of the license is included in the LICENSE file.
-
-date > "${HOME}/.macports/requested.txt"
-port echo requested | cut -d ' ' -f 1 | uniq >> "${HOME}/.macports/requested.txt"
