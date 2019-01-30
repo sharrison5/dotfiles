ab7c61cc707340c0fea843d2fbfd89aeb4843267
Sam Harrison <28861239+sharrison5@users.noreply.github.com>
2019-01-30 10:44:48 +0100

    Include selected ports in MacPorts summary
    I.e. as well as requested ports.

--- macports/.macports/print_status.sh
+++ macports/.macports/print_status.sh
@@ -4,4 +4,8 @@
 # Sam Harrison 2018
 # MIT License: a full version of the license is included in the LICENSE file.
 
+echo "Requested ports"
+echo "==============="
 port echo requested and active
+echo
+port select --summary
