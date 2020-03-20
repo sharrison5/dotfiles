d2b3940b4885f0198debee0168713c56f5521d0d
Sam Harrison <28861239+sharrison5@users.noreply.github.com>
2020-03-20 17:26:11 +0100

    Update MacPorts files
     + Few more ports / variant tweaks.
     + Tidy up of `print_status.sh`, including printing version.

--- macports/.macports/print_status.sh
+++ macports/.macports/print_status.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-# .macports/list_requested.sh
+# .macports/print_status.sh
 # Sam Harrison 2018
 # MIT License: a full version of the license is included in the LICENSE file.
+
+echo "MacPorts"
+echo "========"
+port version
+echo
 
 echo "Requested ports"
 echo "==============="
 port echo requested and active
 echo
+
 port select --summary
